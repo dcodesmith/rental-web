8870376b91ae2cd99817fa12356b1c12fb2703e3
Afees Kolawole <dcodesmith@gmail.com>
2024-01-04 17:41:35 +0100

    comment out api data loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -25,9 +25,9 @@ export default function Index() {
     <div className="flex justify-center">
       <h1 className="mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl font-semibold">Rental Web</h1>
       <div className="flex-col">
-        <pre className="flex">
+        {/* <pre className="flex">
           {JSON.stringify(data, null, 2)}
-        </pre>
+        </pre> */}
       </div>
     </div>
   );
