48f1ff69d8a4a8f8b85531532863ef2e3265e7e5
Afees Kolawole <dcodesmith@gmail.com>
2024-01-04 18:11:23 +0100

    set rental api path

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import { type MetaFunction } from "@remix-run/node";
+import { LoaderFunctionArgs, json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,27 +8,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-// export const loader = async ({
-//   request,
-// }: LoaderFunctionArgs) => {
-//   const url = new URL(request.url);
-//   const q = url.searchParams.get("q");
-
-//   const res = await fetch("http://localhost:3001/api");
-//   const data = await res.json()
-//   return json({ data, q });
-// };
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const apiPath = process.env.RENTAL_API || "http://localhost:3001"
+  const res = await fetch(apiPath);
+  const data = await res.json()
+  return json({ data, q });
+};
 
 export default function Index() {
-  // const { data } = useLoaderData<typeof loader>();
+  const { data } = useLoaderData<typeof loader>();
 
   return (
     <div className="flex justify-center">
       <h1 className="mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl font-semibold">Rental Web</h1>
       <div className="flex-col">
-        {/* <pre className="flex">
+        <pre className="flex">
           {JSON.stringify(data, null, 2)}
-        </pre> */}
+        </pre>
       </div>
     </div>
   );
