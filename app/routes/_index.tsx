0a6be8dbab29ef96f146ad13bd1dfc75566c0112
Afees Kolawole <dcodesmith@gmail.com>
2024-01-04 17:32:44 +0100

    read data from rental-api

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,10 +8,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+
+  const res = await fetch("http://localhost:3001/api");
+  const data = await res.json()
+  return json({ data, q });
+};
+
 export default function Index() {
+  const { data } = useLoaderData<typeof loader>();
+
   return (
     <div className="flex justify-center">
       <h1 className="mb-4 text-4xl leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl font-semibold">Rental Web</h1>
+      <div className="flex-col">
+        <pre className="flex">
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
